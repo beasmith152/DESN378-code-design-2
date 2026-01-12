d3d7a3580c1eca0aa436ce5e55471904dcda72fd
Bea Smith <bsmith152@ewu.edu>
2026-01-12 04:40:11 +0000

    update added the summary, and edited the css

--- week-0/scripts/main0.js
+++ week-0/scripts/main0.js
@@ -47,3 +47,12 @@ function setUserName() {
     myHeading.textContent = `JS is alright, ${myName}`;
   }
 }
+
+/*
+  SUMMARY:
+  This file/exercise does two things:
+  1. The image changer in this manner allows the user to click on the image and have it change to a different image when clicked through the function used above. Its only const is the source, and it's else is conditionally set to the original image.
+  2. The welcome message is standardly set through local storage of what name was used last in case no input is set. Second, if an name is set it changes the content of the h1 where it is positioned to that name. This is all done through the button. 
+​
+  The key pattern I learned: Originally, when setting up week 0, I had trouble with the main.js connecting to the html for this particular exercise due to a main.js existing within my structure due to the original page I already began developing. Therefore, I had to change it's path and name to differentiate to main0.js syntax for the week. However, it did work. Same thing with eliminating the const for the initial heading as the const sets a variable making the third exercise not work entirely. 
+*/
