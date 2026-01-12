0fd379338736a71fe8f88746f505365486dcb73f
Bea Smith <bsmith152@ewu.edu>
2026-01-12 02:49:59 +0000

    update to imagery

--- week-0/scripts/main0.js
+++ week-0/scripts/main0.js
@@ -2,3 +2,16 @@
 const myHeading = document.querySelector("h1");
 // Update the text content of the <h1>
 myHeading.textContent = "Hello world!";
+
+// image portion of assignment
+
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/anime.webp");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+});
