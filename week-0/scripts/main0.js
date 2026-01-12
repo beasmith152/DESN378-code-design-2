964f0275dc4c6abf2afdb626fa5208fab2d3d94c
Bea Smith <bsmith152@ewu.edu>
2026-01-12 03:47:20 +0000

    updated js; eliminated the const for my heading due to change issues.

--- week-0/scripts/main0.js
+++ week-0/scripts/main0.js
@@ -1,7 +1,7 @@
 // Store a reference to the <h1> in a variable
-const myHeading = document.querySelector("h1");
+//const myHeading = document.querySelector("h1");
 // Update the text content of the <h1>
-myHeading.textContent = "Hello world!";
+//myHeading.textContent = "Hello world!";
 
 // image portion of assignment
 
@@ -19,21 +19,19 @@ myImage.addEventListener("click", () => {
 //button portion of assignment
 
 let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
 
-//call
 function setUserName() {
   const myName = prompt("Please enter your name.");
   localStorage.setItem("name", myName);
   myHeading.textContent = `Mozilla is cool, ${myName}`;
 }
-
 if (!localStorage.getItem("name")) {
   setUserName();
 } else {
   const storedName = localStorage.getItem("name");
   myHeading.textContent = `Mozilla is cool, ${storedName}`;
 }
-
 myButton.addEventListener("click", () => {
   setUserName();
 });
